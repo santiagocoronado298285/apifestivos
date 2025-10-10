fc5128b392be98b2f2c92a0b134a34ba02190776
scoronadovasco <scoronadovasco@gmail.com>
2025-10-10 17:30:55 -0500

    refactor: simplificar importaciones en CalendarioController

--- api/src/main/java/festivos/api/controllers/CalendarioController.java
+++ api/src/main/java/festivos/api/controllers/CalendarioController.java
@@ -5,13 +5,8 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import DTOs.FestivosPais;
 import entities.Festivo;
